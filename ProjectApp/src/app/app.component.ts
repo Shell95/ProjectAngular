209246d902afe61fb9fd86bec4a96712a557131e
Shell95 <37145970+Shell95@users.noreply.github.com>
2018-12-01 14:52:45 +0000

    messing with the css

--- ProjectApp/src/app/app.component.ts
+++ ProjectApp/src/app/app.component.ts
@@ -8,3 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lab2-app';
 }
+getUrl() {
+  return "url('https://scontent-dub4-1.xx.fbcdn.net/v/t1.0-9/47271097_2611518348866299_1559992358823526400_n.jpg?_nc_cat=103&_nc_ht=scontent-dub4-1.xx&oh=6e01ee172c18f462c95461bd29bfabbf&oe=5CA06FA9')";
+}
