3c0bc1a6ebe9bba96245e86f34fb643c661ace93
Shell95 <37145970+Shell95@users.noreply.github.com>
2018-12-03 00:48:30 +0000

    finally got it to run
    Forgot to Add import route line to app-routing.module.ts

--- ProjectApp/src/app/app-routing.module.ts
+++ ProjectApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostCreateComponent } from './post-create/post-create.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
+import { RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
